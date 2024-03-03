fb8c3f0cf47b7bfbb2adad2dcc82807f1e43b304
Federico Mederos <fedemederos@MacBook-Pro-de-Federico.local>
2024-03-03 14:46:09 -0300

    casi listo, faltan agregar detalles visuales

---
+++ index.js
@@ -0,0 +1,87 @@
+const container = document.querySelector('.container');
+container.style.display = 'flex';
+container.style.flexDirection = 'column'; 
+// container.style.gap = '2px';
+
+
+const selectSize = document.createElement ('button');
+selectSize.textContent = 'Seleccionar tamaño';
+
+const selectColor = document.createElement ('input');
+selectColor.setAttribute('type', 'color');
+selectColor.setAttribute('id', 'color');
+selectColor.setAttribute('name', 'color');
+
+const resetGrid = document.createElement ('button');
+resetGrid.textContent = 'Reiniciar';
+
+const title = document.createElement ('h1');
+title.textContent = 'Etch-a-Sketch';
+
+const body = document.querySelector('body');
+
+const buttonsContainer = document.createElement ('div');
+buttonsContainer.style.display = 'flex';
+buttonsContainer.style.justifyContent ='center';
+buttonsContainer.style.alignItems = 'center';
+buttonsContainer.style.width = '960px';
+buttonsContainer.style.marginBottom = '10px';
+buttonsContainer.style.gap = '30px';
+
+body.appendChild(title);
+body.insertBefore(title, container);
+body.appendChild(buttonsContainer);
+body.insertBefore(buttonsContainer, container);
+buttonsContainer.appendChild(selectSize);
+buttonsContainer.appendChild(selectColor);
+buttonsContainer.appendChild(resetGrid);
+
+var eliminarElementos;
+
+function agregarFilas(cantidad){
+    for (let i = 0; i < cantidad; i++) {
+        filas = document.createElement ('div');
+        filas.style.height = '100%';
+        filas.style.display = 'flex';
+        filas.classList.add('fila');
+        container.appendChild(filas);
+        for (let j = 0 ; j < cantidad; j++) {
+            columnas = document.createElement ('div');
+            columnas.style.width = '100%';
+            columnas.classList.add ('punto')
+            columnas.style.border = '1px solid lightgray';
+            filas.appendChild(columnas);
+        }
+    eliminarElementos = document.querySelectorAll('.fila')
+}}
+
+const agregarListeners = () => {
+    container.childNodes.forEach ((elemento) => {
+        elemento.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = colorSeleccionado;
+        });
+    });
+};
+
+
+selectSize.addEventListener('click', () => {
+    const tamaño = prompt('Ingrese el tamaño de la grilla');
+    if (tamaño) {
+        agregarFilas(tamaño);
+        agregarListeners();
+    }
+})
+
+let colorSeleccionado;
+selectColor.addEventListener('input', () => {
+    colorSeleccionado = selectColor.value;
+})
+
+resetGrid.addEventListener('click', () => {
+    eliminarElementos.forEach(elemento => {
+        container.removeChild(elemento);
+    });
+});
+
+
+
